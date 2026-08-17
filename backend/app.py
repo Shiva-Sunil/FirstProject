519f416f176ec97ef1c9c5740b33f4948a2db14c
Shiva-Sunil <siva89sunil@gmail.com>
2026-08-16 23:34:31 -0500

    Update backend/app.py

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ import joblib
 from flask import Flask, request, jsonify
 
 #Intialize Flask
-SuperKart_API = Flask('SuperKart Price Predictor')
+SuperKart_api = Flask('SuperKart Price Predictor')
 
 #Load the trained Model 
 model = joblib.load('deployment_files_backend/model.joblib')
@@ -19,8 +19,8 @@ def home():
 def predict_store_sales():
   store_data = request.get_json()
 
-#Retrive date from json
-extract = {
+  #Retrive date from json
+  extract = {
     'Product_Weight': store_data['Product_Weight'],
     'Product_Sugar_Content': store_data['Product_Sugar_Content'],
     'Product_Allocated_Area': store_data['Product_Allocated_Area'],
@@ -31,32 +31,32 @@ extract = {
     'Store_Location_City_Type': store_data['Store_Location_City_Type'],
     'Store_Type': store_data['Store_Type'],
     'Store_Age': store_data['Store_Age']
-}
+  }
 
-#Load data into DF
-extract_data = pd.DataFrame(['extract'])
+  #Load data into DF
+  extract_data = pd.DataFrame(['extract'])
 
-#Predict the sales based on the input by passing the data to model
-predicted_sales = model.predict(extract_data)
+  #Predict the sales based on the input by passing the data to model
+  predicted_sales = model.predict(extract_data)
 
-return jsonify({'Predicted Sales is': float(predicted_sales)})
+  return jsonify({'Predicted Sales is': float(predicted_sales)})
 
 #Post Request for Batch
 @SuperKart_api.post('v1/storebatch')
 def predict_store_sales_batch():
   file = request.files['file']
 
-#Read file
-extract_csv = pd.read_csv(file)
+  #Read file
+  extract_csv = pd.read_csv(file)
 
-#Predict the sales based on the input by passing the data to model
-predicted_sales = model.predict(extract_csv).tolist()
+  #Predict the sales based on the input by passing the data to model
+  predicted_sales = model.predict(extract_csv).tolist()
 
-#Create dict 
-store_id = extract_csv['Store_Id'].tolist()
-predicted_sales_dict = dict(zip(store_id, predicted_sales))
+  #Create dict 
+  store_id = extract_csv['Store_Id'].tolist()
+  predicted_sales_dict = dict(zip(store_id, predicted_sales))
 
-retrun predicted_sales_dict
+  return predicted_sales_dict
 
 # Run the Flask app in debug mode
 if __name__ == '__main__':
